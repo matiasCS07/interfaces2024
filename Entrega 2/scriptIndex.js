dd51b8dea5da3fc79e0bcc5b4217088b9ba9ef73
matiasCS07 <mcresposchmidt@alumnos.exa.unicen.edu.ar>
2024-10-08 16:05:31 -0300

    animacion de los botones login añadida

--- Entrega 2/scriptIndex.js
+++ Entrega 2/scriptIndex.js
@@ -16,40 +16,71 @@ function desplazarCarrusel(carrusel, direccion, destacado) {
     let limiteIzq=0;
     let limiteDer=0;
     let tamanioCard;
-    
-    console.log(destacado, destacado=="carrusel destacado");
+
 
     if(destacado=="carrusel destacado"){
         if(screen.width>1000){
-            limiteDer=10
+            limiteDer=5
         }else{
-            limiteDer=14;
+            limiteDer=7;
         }
         tamanioCard=document.querySelector(".carrusel.destacado .card").getBoundingClientRect().width;
     }else{
         if(screen.width>1000){
-            limiteDer=4;
+            limiteDer=2;
         }else{
-            limiteDer=8;
+            limiteDer=4;
         }
         tamanioCard=document.querySelector(".carrusel.normal .card").getBoundingClientRect().width;
     }
 
-    console.log(tamanioCard)
-
-    let desplazamiento=tamanioCard;
+    let desplazamiento=tamanioCard*2;
     let carruselPos=parseInt(carrusel.getAttribute("data-posicion"));
 
     if (direccion == 'next') {
         if(!(carruselPos>=limiteDer)){
             carruselPos+=1;
+            carrusel.style.transformOrigin='left';
         }
     } else {
         if(!(carruselPos<=limiteIzq)){
             carruselPos-=1;
+            carrusel.style.transformOrigin='right';
         }
     }
-    
+
     carrusel.setAttribute('data-posicion',`${carruselPos}`);
-    carrusel.style.transform=`translate(${(carruselPos*desplazamiento)*-1}px, 0px)`;
+
+    /* animacion con deformacion de rombo skew
+        carrusel.style.transform=`skewX(25deg) scaleY(1.15) translate(${(carruselPos*desplazamiento)*-1}px, 0px)`;
+        setTimeout(() => {
+            carrusel.style.transform=`skewX(15deg) scaleY(1.075) translate(${(carruselPos*desplazamiento)*-1}px, 0px)`;
+        }, 10);
+        setTimeout(() => {
+            carrusel.style.transform=`skewX(10deg) scaleY(1.05) translate(${(carruselPos*desplazamiento)*-1}px, 0px)`;
+        }, 200);
+        setTimeout(() => {
+            carrusel.style.transform=`skewX(5deg) scaleY(1.005) translate(${(carruselPos*desplazamiento)*-1}px, 0px)`;
+        }, 300);
+        setTimeout(() => {
+            carrusel.style.transform=`skewX(0deg) scaleY(1) translate(${(carruselPos*desplazamiento)*-1}px, 0px)`;
+        }, 400);
+    */
+
+    /* animacion de scale */
+
+
+    carrusel.style.transform=`skewX(10deg) scaleX(1.15) scaleY(1.075) translate(${(carruselPos*desplazamiento)*-1}px, 0px)`;
+    setTimeout(() => {
+        carrusel.style.transform=`skewX(5deg) scaleX(1.10) scaleY(1.05) translate(${(carruselPos*desplazamiento)*-1}px, 0px)`;
+    }, 10);
+    setTimeout(() => {
+        carrusel.style.transform=`skewX(2.5deg) scaleX(1.05) scaleY(1.025) translate(${(carruselPos*desplazamiento)*-1}px, 0px)`;
+    }, 200);
+    setTimeout(() => {
+        carrusel.style.transform=`skewX(1deg) scaleX(1.02) scaleY(1.01) translate(${(carruselPos*desplazamiento)*-1}px, 0px)`;
+    }, 300);
+    setTimeout(() => {
+        carrusel.style.transform=`skewX(0deg) scaleX(1) scaleY(1) translate(${(carruselPos*desplazamiento)*-1}px, 0px)`;
+    }, 400);
 }
