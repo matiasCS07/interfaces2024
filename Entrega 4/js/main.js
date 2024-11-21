ceb6aab28619866d211b478a0cc8ef934765b66d
IgnacioGiordanoMargni <icasas760@gmail.com>
2024-11-21 18:17:26 -0300

    el cero aparece en la seccion larga

--- Entrega 4/js/main.js
+++ Entrega 4/js/main.js
@@ -173,7 +173,7 @@ const seccionLarga = document.querySelector('.sticky-slider');
 const seccionesTexto = document.querySelectorAll(".contenedor-dato");
 const seccionesTextoLimits=document.querySelector(".contenedor-datos").getBoundingClientRect();
 let imagen=document.querySelector(".seccion-larga-0");
-let seccionActual;
+let seccionActual="0";
 
 
 window.addEventListener('scroll', () => {
@@ -184,9 +184,8 @@ window.addEventListener('scroll', () => {
       const seccionLimites = seccion.getBoundingClientRect();
       const centroSeccion=seccionLimites.top+(seccionLimites.height/2);
       const distancia=Math.abs(centroVentana-centroSeccion); //distancia entre el centro de la seccion y el de la ventana
-      
       if (distancia<(seccionLimites.height)) {
-        if(seccionActual!=seccion.getAttribute("data-imagen")){
+        if(seccionActual!=seccion.getAttribute("data-imagen") || seccionActual=="0"){
           seccionActual=seccion.getAttribute("data-imagen");
           imagen.classList.add("reveal");
           imagen.classList.add("fixed");
@@ -195,6 +194,10 @@ window.addEventListener('scroll', () => {
           imagen.classList.remove("fixed");
         }
         imagen.src=`assets/images/scroll-personajes/${seccionActual}.png`;
+        console.log(seccionActual);
+
+      } else if(distancia>(seccionLimites.top) || distancia<(seccionLimites.bottom)) {
+        imagen.classList.remove("reveal");
       }
     });
 });
